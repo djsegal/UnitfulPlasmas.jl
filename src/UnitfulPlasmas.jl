185974a6f90e0bea147e77c3458a560b41c260cd
Dan Segal <danSegal2@gmail.com>
2017-07-17 16:45:57 -0400

    Clean up syntax in main package file

--- src/UnitfulPlasmas.jl
+++ src/UnitfulPlasmas.jl
@@ -1,19 +1,22 @@
 __precompile__(true)
+
 module UnitfulPlasmas
 
-import Unitful
-using Unitful: @unit
+  import Unitful
+  using Unitful: @unit
 
-import Unitful.DefaultSymbols
+  import Unitful.DefaultSymbols
 
-@unit n20 "n20" PlasmaDensity 1e20/Unitful.m^3 false
+  @unit n20 "n20" PlasmaDensity 1e20/Unitful.m^3 false
 
-# Some gymnastics required here because if we precompile, we cannot add to
-# Unitful.basefactors at compile time and expect the changes to persist to runtime.
-const localunits = Unitful.basefactors
-function __init__()
-    merge!(Unitful.basefactors, localunits)
-    Unitful.register(UnitfulPlasmas)
+  # Some gymnastics required here because if we precompile, we cannot add to
+  # Unitful.basefactors at compile time and expect the changes to persist to runtime.
+
+  const localunits = Unitful.basefactors
+
+  function __init__()
+      merge!(Unitful.basefactors, localunits)
+      Unitful.register(UnitfulPlasmas)
+  end
+
 end
-
-end # module
