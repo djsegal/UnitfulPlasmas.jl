104e5fe92b23e27155f633366925da162b0b93c2
Dan Segal <danSegal2@gmail.com>
2017-07-17 16:39:09 -0400

    Update project to be Unitful for Plasmas

---
+++ src/UnitfulPlasmas.jl
@@ -0,0 +1,19 @@
+__precompile__(true)
+module UnitfulPlasmas
+
+import Unitful
+using Unitful: @unit
+
+import Unitful.DefaultSymbols
+
+@unit n20 "n20" PlasmaDensity 1e20/Unitful.m^3 false
+
+# Some gymnastics required here because if we precompile, we cannot add to
+# Unitful.basefactors at compile time and expect the changes to persist to runtime.
+const localunits = Unitful.basefactors
+function __init__()
+    merge!(Unitful.basefactors, localunits)
+    Unitful.register(UnitfulPlasmas)
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,2 @@
-using UnitfulUS
+using UnitfulPlasmas
 using Base.Test
-
-@testset "US string macro" begin
-    @test macroexpand(:(us"gal")) == :(UnitfulUS.ustrcheck(UnitfulUS.gal_us))
-    @test macroexpand(:(us"1.0")) == 1.0
-    @test macroexpand(:(us"ton/gal")) == :(UnitfulUS.ustrcheck(UnitfulUS.ton_us)
-        / UnitfulUS.ustrcheck(UnitfulUS.gal_us))
-    @test macroexpand(:(us"1.0gal")) ==
-        :(1.0 * UnitfulUS.ustrcheck(UnitfulUS.gal_us))
-    @test macroexpand(:(us"gal^-1")) ==
-        :(UnitfulUS.ustrcheck(UnitfulUS.gal_us) ^ -1)
-
-    @test isa(macroexpand(:(us"ton gal")).args[1], ParseError)
-
-    # Disallowed functions
-    @test isa(macroexpand(:(us"abs(2)")).args[1], ErrorException)
-
-    # Units not found
-    @test isa(macroexpand(:(us"kg")).args[1], ErrorException)
-
-    # test ustrcheck(x) fallback to catch non-units / quantities
-    @test isa(macroexpand(:(us"ustrcheck")).args[1], ErrorException)
-end
